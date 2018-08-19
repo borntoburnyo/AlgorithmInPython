0eccd1559f1a89a7b59b6b861d693f3cd069ec0e
Demo <demodemowhitebig@gmail.com>
2018-08-18 21:10:46 -0400

    Update 867 Transpose Matrix.py

--- Easy/867 Transpose Matrix.py
+++ Easy/867 Transpose Matrix.py
@@ -2,8 +2,6 @@ Given a matrix A, return the transpose of A.
 
 The transpose of a matrix is the matrix flipped over it's main diagonal, switching the row and column indices of the matrix.
 
- 
-
 Example 1:
 
 Input: [[1,2,3],[4,5,6],[7,8,9]]
@@ -19,3 +17,20 @@ Note:
 1 <= A.length <= 1000
 1 <= A[0].length <= 1000
 
+class Solution:
+ def transpose(self, A):
+  """
+  dtype A: List[List[int]]
+  rtype: List[List[int]]
+  """
+  n, L = len(A), len(A[0])
+  newMat = [[0]*n for i in range(L)]
+  
+  for i in range(n):
+   for j in range(L):
+    newMat[j][i] = A[i][j]
+   
+  return newMat
+ 
+ def alternative(self, A):
+  return list(zip(*A))
