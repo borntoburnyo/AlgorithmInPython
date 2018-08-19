76e6cff89842f7e97f7bd9a3f90b2a49259caadd
Demo <demodemowhitebig@gmail.com>
2018-08-19 16:17:30 -0400

    Update 1 Two Sum.py

--- Easy/1 Two Sum.py
+++ Easy/1 Two Sum.py
@@ -9,3 +9,19 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 
+class Solution:
+	def twoSum(self, nums, target):
+		"""
+		dtype nums: List[int]
+		dtype target: int
+		rtype: List[int]
+		"""
+		if len(nums) < 1:
+			return False 
+		for i in range(len(nums)-1):
+			for j in range(i+1, len(nums)):
+				if nums[i] + nums[j] == target:
+					return [i,j]
+	
+	def alternativeOne(self, nums, target):
+		
