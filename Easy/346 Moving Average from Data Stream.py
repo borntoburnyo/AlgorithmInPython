1e4c7f0693458eb05c80919fd8807ac73501fd54
Demo <demodemowhitebig@gmail.com>
2018-10-16 22:03:28 -0400

    Create 346 Moving Average from Data Stream.py

---
+++ Easy/346 Moving Average from Data Stream.py
@@ -0,0 +1,17 @@
+Description: 
+
+Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+
+class MovingAverage(object)L:
+  def __init__(self, size):
+    self.size = size
+    self.queue = []
+  
+  def next(self, val):
+    if not self.queue or len(self.queue) < self.size:
+      self.queue.append(val)
+    else:
+      self.queue.pop(0)
+      self.queue.append(val)
+      
+    return float(sum(self.queue)) / len(self.queue)
