4735f4fe927f20119a31a00f9e1de9d125e66950
Demodemo <jiangtaotao2@gmail.com>
2022-02-13 11:55:01 -0500

    Utilize python heap priority queue

--- easy/1337_the_k_weakest_rows_in_a_matrix.py
+++ easy/1337_the_k_weakest_rows_in_a_matrix.py
@@ -4,3 +4,16 @@ class Solution:
 		# utilize the built-in sort feature in Python
 		self.nums.sort()
 		return [x for i,x in self.nums[:k]]
+
+	def kWeakestRows(self, mat, k):
+		# utilize heap priority queue
+		self.nums = [(sum(x),i) for i,x in enumerate(mat)]
+		self.k = k
+		heapq.heapify(self.nums)		
+		idx = []
+		while self.k > 0:
+			strength, i = heapq.heappop(self.nums)
+			idx.append(i)
+			k -= 1
+		
+		return idx
