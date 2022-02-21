c535787a44be1fa894c8bcfc3b6ac6aa007e2479
Demodemo <jiangtaotao2@gmail.com>
2022-02-21 15:03:46 -0500

    Avoid use min/max function

--- python_implementation/min_max_comparison.py
+++ python_implementation/min_max_comparison.py
@@ -1,14 +1,16 @@
 def findMinMax(nums):
 	if len(nums) // 2 == 0:
-		R = [min(nums[0], nums[1])]*2
+		R = [nums[0] if nums[0] < nums[1] else nums[1]]*2
 	else:
 	    R = [nums[0]]*2
 
 	for i in range(1,len(nums)-1, 2):
-		small = min(nums[i], nums[i+1])
-		R[0] = min(small, R[0])	
-		large = max(nums[i], nums[i+1])
-		R[1] = max(large, R[1])
+		if nums[i] < nums[i+1]:
+			R[0] = R[0] if R[0] < nums[i] else nums[i]
+			R[1] = R[1] if R[1] > nums[i+1] else nums[i+1]
+		else:
+			R[0] = R[0] if R[0] < nums[i+1] else nums[i+1]
+			R[1] = R[1] if R[1] > nums[i] else nums[i]
 
 	return R
 
