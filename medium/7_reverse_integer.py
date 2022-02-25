307e7fbbd0f331143ac4ece503fe8d28e330d524
Demodemo <jiangtaotao2@gmail.com>
2022-02-25 16:16:04 -0500

    x==(x//10)*10 + x%10

---
+++ medium/7_reverse_integer.py
@@ -0,0 +1,11 @@
+class Solution:
+	def reverse(self, x):
+		sign = -1 if x < 0 else 1
+		if x < 0:
+			x = -x
+		rev = 0
+		while x != 0:
+			rev = rev * 10 + x % 10
+			x = x // 10
+			return 0 if rev > 2**31 - 1
+		return sign*rev
