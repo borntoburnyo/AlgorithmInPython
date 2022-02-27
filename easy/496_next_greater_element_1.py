38bb1bd328d1f1aa3d492201d06d174da1112d5c
Demodemo <jiangtaotao2@gmail.com>
2022-02-26 21:58:32 -0500

    Store index to avoid issue caused by duplicates

--- easy/496_next_greater_element_1.py
+++ easy/496_next_greater_element_1.py
@@ -2,7 +2,7 @@ class Solution:
 	def nextGreaterElement(self, nums1, nums2):
 		hmap, stk = {}, []
 		for x in nums2:
-			if not stk or stk[-1] >= x:
+			if not stk and stk[-1] >= x:
 				stk.append(x)
 			else:
 				while stk and stk[-1] < x:
@@ -10,10 +10,29 @@ class Solution:
 				stk.append(x)
 
 		res = []
-		for x in num1:
+		for x in nums1:
 			if x in hmap:
 				res.append(hmap[x])
 			else:
 				res.append(-1)
 		
 		return res
+    
+    def v2(self, nums1, nums2):
+        hmap, stk = {}, []
+        for i in range(len(nums2)):
+            if not stk and nums2[stk[-1]] < nums2[i]:
+                stk.append(i) 
+            else:
+                while stk and nums2[stk.pop()] < nums2[i]:
+                    hmap[stk.pop()] = nums2[i]
+                stk.append(i)
+        
+        res = []
+        for x in nums1:
+            if nums2.index(x) in hmap:
+                res.append(hmap[nums2.index(x)])
+            else:
+                res.append(-1)
+
+        return res
