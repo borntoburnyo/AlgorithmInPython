888115b549bfb40821bd2487d2a7a838bd840820
Demodemo <jiangtaotao2@gmail.com>
2022-02-28 15:56:39 -0500

    Find pattern then solve

---
+++ easy/13_roman_to_integer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def romanToInt(self, s):
+        hmap = dict(zip(['I','V','X','L','C','D';'M'], [1,5,10,50,100,500,1000]))
+        res = 0
+        for i in range(len(s)-1):
+            if hmap[s[i+1]] > hmap[s[i]]:
+                res -= hmap[s[i]]
+            else:
+                res += hmap[s[i]]
+
+        return res + hmap[s[-1]]
