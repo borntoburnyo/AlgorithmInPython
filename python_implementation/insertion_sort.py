7dec6baaadfcad43ca00aa7518d9fa1ae7b2b432
Demodemo <jiangtaotao2@gmail.com>
2022-03-05 20:18:00 -0500

    Add comment explainning the idea

--- python_implementation/insertion_sort.py
+++ python_implementation/insertion_sort.py
@@ -2,6 +2,9 @@ def InsertionSort(nums):
 	for j in range(1,len(nums)):
 		key = nums[j]
 		i = j - 1
+        # Make sure elements with index < j stay increasing order
+        # O/W, switch to push the biggest to the position of j-1
+        # Just imagine how you shuffle a hand of pokers
 		while i > -1 and nums[i] > key:
 			nums[i+1], nums[i] = nums[i], nums[i+1]
 			i -= 1
