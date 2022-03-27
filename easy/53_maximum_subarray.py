359eebf942c13e1672210eec9638636a4d273eb9
Demodemo <jiangtaotao2@gmail.com>
2022-03-27 14:52:09 -0400

    Divide and conquer added

--- easy/53_maximum_subarray.py
+++ easy/53_maximum_subarray.py
@@ -5,3 +5,29 @@ class Solution:
             if nums[i-1] > 0:
                 nums[i] += nums[i-1]
         return max(nums)
+
+    def maxSubArray(self, nums):
+        def findOptimalSub(nums, left, right):
+            if left > right:
+                return -math.inf
+            else:
+                mid = (left + right) // 2
+                cur = leftHalf = rightHalf = 0
+
+                for i in range(mid-1, left-1, -1):
+                    cur += nums[i]
+                    leftHalf = max(leftHalf, cur)
+
+                cur = 0
+                for i in range(mid+1, right+1, 1):
+                    cur += nums[i]
+                    rightHalf = max(rightHalf, cur)
+
+                crossMax = nums[mid] + leftHalf + rightHalf
+
+                leftMax = findOptimalSub(nums, left, mid - 1)
+                rightMax = findOptimaSub(nums, mid + 1, right)
+                
+                return max(crossMax, leftMax, rightMax)
+
+        return findOptimalSub(nums, 0, len(nums) - 1)
