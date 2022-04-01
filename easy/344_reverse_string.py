a57ad4f1368bfced020af9f5d5fc7a7886f82c46
Demodemo <jiangtaotao2@gmail.com>
2022-03-31 22:48:48 -0400

    Use two pointer to reverse a list

---
+++ easy/344_reverse_string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseString(self, s):
+        i, j = 0, len(s) - 1
+        while i <= j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+        
+        return s
+
+# Use two pointer to reverse a list
